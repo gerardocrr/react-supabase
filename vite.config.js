51920f7d1316a100f5c897017f9fd6be752779cc
gerardocrr <gerardo2551.cr@gmail.com>
2024-09-12 20:06:22 -0600

    added file and variables .env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
