b45a2133544d712c1af7b78df53c72e43b4bd0fb
gerardocrr <gerardo2551.cr@gmail.com>
2024-09-12 20:55:43 -0600

    adedd code to get data clients

--- src/Pages/Clients.jsx
+++ src/Pages/Clients.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { supabase } from "@/api/supabase";
+
 export function Clients() {
+  const [dataClients, setDataClients] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("clients").select();
+      if (error) {
+        console.log("Error fetching data:", error.message);
+      } else {
+        setDataClients(data);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Clients</h1>
+      {dataClients.map((client) => (
+        <li key={client.id}>{client.name}</li>
+      ))}
     </div>
   );
 }
